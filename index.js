b1f3442d3aacb8d9430c45e5dfa2962619fd13e5
mmelland2 <mmelland2@student.cvtc.edu>
2021-11-02 07:28:19 -0500

    Fixed errors with JSHint

--- index.js
+++ index.js
@@ -3,104 +3,100 @@ const path = require('path');
 const PORT = process.env.PORT || 5000;
 const { Pool } = require('pg');
 
-const pool = new Pool({
-    connectionString: process.env.DATABASE_URL,
-    ssl: {
-        rejectUnauthorized: false
-    }
+const pool = new Pool ({
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false
+  }
+
 });
 
 express()
-    .use(express.static(path.join(__dirname, 'public')))
-    .use(express.json())
-    .use(express.urlencoded({ extended: true}))
-    .set('views', path.join(__dirname, 'views'))
-    .set('view engine', 'ejs')
-    .get('/', async(req, res) => {
-        try {
-            const client = await pool.connect();
+  .use(express.static(path.join(__dirname, 'public')))
+  .use(express.json())
+  .use(express.urlencoded({ extended: true}))
+  .set('views', path.join(__dirname, 'views'))
+  .set('view engine', 'ejs')
+  .get('/', async(req, res) => {
+    try {
+    const client = await pool.connect();
 
-            const tasks = await client.query(
-                `SELECT * FROM tasks ORDER BY id ASC`
-            );
+    // make some buttons
+    const tasks = await client.query(
+      `SELECT * FROM tasks ORDER BY id ASC;`
+    );
 
-            const locals = {
-                'tasks': (tasks) ? tasks.rows : null
-            };
+    const locals = {
+      'tasks': (tasks) ? tasks.rows : null
+    };
+    res.render('pages/index', locals);
+    client.release();
+    }
+    catch (err) {
+      console.error(err);
+      res.send("Error: " + err);
+    }
+  })
+  .get('/db-info', async(req, res) => {
+    try {
+      const client = await pool.connect();
+      const tables = await client.query(
 
-            res.render('pages/index', locals);
-            client.release();
-        }
-        catch (err) {
-            console.error(err);
-            res.send("Error " + err);
-        }
-    })
-    .get('/db-info', async(req, res) => {
-        try {
-            const client = await pool.connect();
-            const tables = await client.query(
-                `SELECT c.relname AS table, a.attname AS column, t.typname AS type
-                FROM pg_catalog.pg_class AS c
-                LEFT JOIN pg_catalog.pg_attribute AS a
-                ON c.oid = a.attrelid AND a.attnum > 0
-                LEFT JOIN pg_catalog.pg_type AS t
-                ON a.atttypid = t.oid
-                WHERE c.relname IN ('users', 'observations', 'students', 'schools', 'tasks')
-                ORDER BY c.relname, a.attnum;
-                `);
+      `SELECT c.relname AS table, a.attname AS column, t.typname AS type
+      FROM pg_catalog.pg_class AS c
+      LEFT JOIN pg_catalog.pg_attribute AS a
+      ON c.oid = a.attrelid AND a.attnum > 0
+      LEFT JOIN pg_catalog.pg_type AS t
+      ON a.atttypid = t.oid
+      WHERE c.relname IN ('users', 'observations', 'students', 'schools', 'tasks')
+      ORDER BY c.relname, a.attnum
+      `);
 
-                const obs = await client.query( `SELECT * FROM observations`);
-                
+      const obs = await client.query(
+        `SELECT * FROM observations`
+      );
 
-                const locals = {
-                    'tables': (tables) ? tables.rows : null,
-                    'obs': (obs) ? obs.rows : null
-                };
+      const locals = {
+        'tables': (tables) ? tables.rows : null,
+        'obs': (obs) ? obs.rows : null
+      };
 
-                res.render('pages/db-info', locals);
-                client.release();
+      res.render('pages/db-info', locals);
+      client.release();
+    }
+    catch (err) {
+      console.error(err);
+      res.send("Error! " + err);
+    }
 
+  })
+  .post('/log', async(req, res) => {
+    try {
+      const client = await pool.connect();
 
+      const usersId = req.body.users_id;
+      const studentId = req.body.students_id;
+      const tasksId = req.body.tasks_id;
+      const duration = req.body.duration;
 
-        } 
-        catch (err) {
-            console.error(err);
-            res.send("Error " + err);
+      const sqlInsert = await client.query(
+`INSERT INTO observations (users_id, students_id, tasks_id, duration)
+VALUES (${usersId}, ${studentId}, ${tasksId}, ${duration})
+RETURNING id AS new_id;`);
+      console.log(`Tracking task ${tasksId}`);
 
-        }
-    })
-    .post('/log', async(req, res) => {
-        try {
-
-            const client = await pool.connect();
-            const usersId = req.body.users_id;
-            const studentsId = req.body.students_id
-            const tasksId = req.body.task_id;
-            const duration = req.body.duration;
-
-            const sqlInsert = await client.query(
-                `INSERT INTO observations (users_id, students_id, tasks_id, duration)
-                VALUES (${usersId}), ${studentsId}, ${tasksId}, ${duration})
-                RETURNING id AS new_id;`);
-            console.log(`Tracking task ${tasksId}`);
-
-            const result = {
-                'response': (sqlInsert) ? (sqlInsert.rows[0]) : null
-            };
-
-            res.set({
-                'Content-Type': 'application/json'
-            });
-            res.json({ requestBody: result });
-            client.release();
-            
-
-        } catch (eff) {
-            console.error(err);
-            res.send("Error " + err);
-        }
-
-    })
-    .listen(PORT, () => console.log(`Listening on ${ PORT }`));
-
+      const result = {
+        'response': (sqlInsert) ? (sqlInsert.rows[0]) : null
+      };
+      res.set({
+        'Content-type': 'application/json'
+      });
+      res.json({ requestBody: result });
+      client.release();
+    }
+    catch (err) {
+      console.error(err);
+      res.send("Error! " + err);
+    }
+  })
+  .listen(PORT, () => console.log(`Listening on ${ PORT }`));
